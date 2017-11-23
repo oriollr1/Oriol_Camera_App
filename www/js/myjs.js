1214e5254a129ce4d6365824e6d4a8d585c3a403
Uri i <oriol.lopez.reina@gmail.com>
2017-11-23 23:58:15 +0100

    first commit

---
+++ www/js/myjs.js
@@ -0,0 +1,17 @@
+function opencamera(){
+navigator.camera.getPicture(onSuccess, onFail, { quality: 50,
+    destinationType: Camera.DestinationType.FILE_URI });
+
+function onSuccess(imageURI) {
+    var image = document.getElementById('myImage');
+    image.src = imageURI;
+}
+
+function onFail(message) {
+    alert('Failed because: ' + message);
+}
+}
+
+function init(){
+ document.getElementById("mybutton").addEventListener("click",opencamera);
+}
